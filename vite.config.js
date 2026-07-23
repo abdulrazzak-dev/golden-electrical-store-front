10da0da75783474cb27c84709acb08d8b3f3e26d
AHARazzak <hameedabdulrazzak@gmail.com>
2026-07-23 17:32:03 +0530

    correct thr error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/golden-electrical-store-front/', // ⚠️ உங்கள் GitHub Repository பெயரை 'your-repository-name' இடத்தில் இடவும்
+  plugins: [react()],
+  base: '/golden-electrical-store-front/', // ⚠️ இரண்டு பக்கமும் சாய்வு கோடு (slash) இருக்க வேண்டும்
 })
