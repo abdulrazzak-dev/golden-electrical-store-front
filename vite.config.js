75f963f369d8054d2489f8be8921cc5654799c8b
AHARazzak <hameedabdulrazzak@gmail.com>
2026-07-23 21:30:04 +0530

    correct error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // ⚠️ இரண்டு பக்கமும் சாய்வு கோடு (slash) இருக்க வேண்டும்
+  base: '/golden-electrical-store-front/', // ⚠️ இரண்டு பக்கமும் சாய்வு கோடு (slash) இருக்க வேண்டும்
 })
