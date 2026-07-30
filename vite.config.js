6d7d8c9c2e014417e26df72fbdf300a69912a048
AHARazzak <hameedabdulrazzak@gmail.com>
2026-07-30 14:57:52 +0530

    correct the error

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/golden-electrical-store-front/', // ⚠️ இரண்டு பக்கமும் சாய்வு கோடு (slash) இருக்க வேண்டும்
 })
